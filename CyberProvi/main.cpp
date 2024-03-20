d3dbc8a9e29209279b148ba299440e8957283707
Bastien Fontagne <fontagne.bastien1@gmail.com>
2024-03-20 18:32:48 +0100

    Ajoutez des fichiers projet.

---
+++ CyberProvi/main.cpp
@@ -0,0 +1,21 @@
+#include "Personnage.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void afficherMenu() {
+    std::cout << "Actions disponibles :" << std::endl;
+    std::cout << "1. Attaquer" << std::endl;
+    std::cout << "2. Lancer un sort" << std::endl;
+    std::cout << "3. Boire une potion de vie (+20PV)" << std::endl;
+    std::cout << "4. Boire une potion de mana (+50Mana)" << std::endl;
+    std::cout << "5. Quitter le jeu" << std::endl;
+    std::cout << "Choisissez une action : ";
+}
+
+int main()
+{
+    
+}
+
